6412313dcc6ed5ba7680b69f495598c74dcf5c7d
a.breezy <ambrose.wilkins@gmail.com>
2022-02-28 13:46:59 -0500

    add server and create User model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+	username: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		validate: {
+			email,
+		},
+	},
+	// references Thought model _id
+	thoughts: {},
+	// self-referencial to User model _id
+	friends: {},
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
